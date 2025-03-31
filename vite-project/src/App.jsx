1cb71ee362e3f4dc1649e41aa26e18f570adb7ea
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-03-31 13:34:26 +0330

    home design and navlink

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
+import { BiHome } from 'react-icons/bi'
+import Home from './Components/Home'
 
 const App = () => {
+
+  // const [data, setData] = useState([])
+  // const [loadind, setLoading] = useState(false)
+  // const [errorMsg, setErrorMsg] = useState(null)
+
+  // const fetchData = async () => {
+  //   const response = await fetch('https://api.potterdb.com/v1/')
+  //   const data = await response.json()
+  //   console.log(data)
+  //   if(data) {
+  //     setLoading(false)
+  //     setData(data)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   if(!data || data.length === 0) {
+  //     fetchData()
+  //   }
+  // },[])
+
   return (
-    <div className='text-5xl'>App</div>
+    <div className='text-5xl'>
+      <Router>
+        <nav className='bg-green-950 text-white'>
+        <NavLink to='/' className={ ({ isActive }) => isActive ? 'text-black 2xl:' : 'text-gray-800'}>Home <BiHome/></NavLink>
+        </nav>
+    
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ vite-project/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <img alt='feed' src='/public/feed.jpg' className='w-screen h-screen object-cover'/>
+    </div>
+  )
+}
+
+export default Home
