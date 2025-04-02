7c7d75758279bf3660a41192335426d5ac83d672
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-04-02 19:00:48 +0330

    complete search field

--- vite-project/src/Components/HogwartsStaff.jsx
+++ vite-project/src/Components/HogwartsStaff.jsx
@@ -2,9 +2,13 @@ import { RingLoader } from 'react-spinners'
 import StaffCard from './StaffCard'
 import React, { useEffect, useState } from 'react'
 
-const HogwartsStaff = () => { const [data, setData] = useState([]) 
+const HogwartsStaff = () => { 
+    
+const [data, setData] = useState([]) 
 const [loading, setLoading] = useState(false)
 const [errorMsg, setErrorMsg] = useState(null)
+const [input, setInput] = useState('')
+const [filteredData, setFilteredData] = useState([])
 
 const fetchData = async () => {
     try {
@@ -13,6 +17,7 @@ const fetchData = async () => {
         const result = await response.json()
         if(result && result.length) {
             setData(result)
+            setFilteredData(result)
             setLoading(false)
             console.log(data)
         }
@@ -22,23 +27,40 @@ const fetchData = async () => {
     }
 }
 
+
 useEffect(() => {
-    if(!data || data.length === 0) {
+    if(!data || data.length === 0 ) {
         fetchData()
     }}, [])
 
+    const handleSearch = () => {
+        if(input.trim() !== '') {
+            const newData = data.filter((item, index) => (
+                item.name.toLowerCase().includes(input.toLowerCase())
+            ))
+            setFilteredData(newData)
+            setInput('')
+        } else {
+            setFilteredData(data)
+        }
+    }
+
 return (
-<div className='grid grid-cols-4 gap-2 p-2'>
+<div>
+<div className='flex px-2 text-sm gap-2 mt-4'>
+    <input type='text' placeholder='Find a Staff Member…' 
+    className='w-52 p-2 border-gray-400 border-1 rounded-md'
+    value={input} onChange={(e) => setInput(e.target.value)}/>
+    <button className='p-2 bg-green-500 text-gray-800 rounded-md' onClick={handleSearch}>Search</button>
+    </div>
+<div className='grid grid-cols-4 gap-2 p-2 mt-2'>
     {errorMsg && <p className='text-red-600'>{errorMsg}</p>}
     {loading && <div className='w-screen h-screen flex items-center justify-center'><RingLoader size={150}/> </div>}
     {
-        data.map((item, index) => (
+        filteredData.map((item, index) => (
             <StaffCard key={index} item={item} />
-        ))
-    }
-    
+        ))}   
 </div>
-)
-}
+</div>)}
 
 export default HogwartsStaff
--- vite-project/src/Components/Spell.jsx
+++ vite-project/src/Components/Spell.jsx
@@ -6,6 +6,8 @@ const Spell = () => {
     const [data, setData] = useState([]) 
     const [loading, setLoading] = useState(false)
     const [errorMsg, setErrorMsg] = useState(null)
+    const [filteredData, setFilteredData] = useState([])
+    const [input, setInput] = useState('')
 
     const fetchData = async () => {
         try {
@@ -14,6 +16,7 @@ const Spell = () => {
             const result = await response.json()
             if(result && result.length) {
                 setData(result)
+                setFilteredData(result)
                 setLoading(false)
                 console.log(data)
             }
@@ -23,22 +26,41 @@ const Spell = () => {
         }
     }
 
+    const handleSearch = () => {
+        if(input.trim() !== 0) {
+            const newFilteredData = data.filter((item) => (
+                item.name.toLowerCase().includes(input.toLowerCase())
+            ))
+            setFilteredData(newFilteredData)
+            setInput('')
+        } else {
+            setFilteredData(data)
+        }
+    }
+
     useEffect(() => {
         if(!data || data.length === 0) {
             fetchData()
         }}, [])
  
   return (
+    <div>
+    <div className='flex px-2 text-sm gap-2 mt-4'>
+    <input type='text' placeholder='Find your favorite character…' 
+    className='w-52 p-2 border-gray-400 border-1 rounded-md'
+    value={input} onChange={(e) => setInput(e.target.value)}/>
+    <button className='p-2 bg-green-500 text-gray-800 rounded-md' onClick={handleSearch}>Search</button>
+    </div>
     <div className='grid grid-cols-4 gap-2 p-2'>
         {errorMsg && <p className='text-red-600'>{errorMsg}</p>}
         {loading && <div className='w-screen h-screen flex items-center justify-center'><RingLoader size={150}/> </div>}
         {
-            data.map((item, index) => (
+            filteredData.map((item, index) => (
                 <SpellCard key={index} item={item} />
-            ))
-        }
-        
+            ))}  
     </div>
+    </div>
+
   )
 }
 
