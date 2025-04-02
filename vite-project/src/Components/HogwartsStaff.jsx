54b0a5c9197d0157e43b334f97861aaa9d2dbbb9
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-04-02 19:30:45 +0330

    start favorite state
    adding a button for favorite users, with icon & other stuff

--- vite-project/src/Components/HogwartsStaff.jsx
+++ vite-project/src/Components/HogwartsStaff.jsx
@@ -1,6 +1,34 @@
 import { RingLoader } from 'react-spinners'
-import StaffCard from './StaffCard'
 import React, { useEffect, useState } from 'react'
+import { FaRegHeart, FaHeart } from 'react-icons/fa'
+
+const characterObject = {
+    'Galatea Merrythought' : '/public/images/ Galatea Merrythought.webp',
+    'Madame Pince' : '/public/images/ Madame Pince.webp',
+    'Pomona Sprout' : '/public/images/ Pomona Sprout.webp',
+    'Severus Snape' : '/public/images/ Severus Snape.webp',
+    'Alastor Moody' : '/public/images/Alastor Moody.jpg',
+    'Albus Dumbledore' : '/public/images/Albus Dumbledore.jpg',
+    'Argus Filch' : '/public/images/Argus Filch.jpeg',
+    'Aurora Sinistra' : '/public/images/Aurora Sinistra.jpg',
+    'Charity Burbage' : '/public/images/Charity Burbage.jpg',
+    'Cuthbert Binns' : '/public/images/Cuthbert Binns.jpg',
+    'Dolores Umbridge' : '/public/images/Dolores Umbridge.webp',
+    'Filius Flitwick' : '/public/images/Filius Flitwick.webp',
+    'Firenze' : '/public/images/Firenze.jpg',
+    'Gilderoy Lockhart' : '/public/images/Gilderoy Lockhart.webp',
+    'Horace Slughorn' : '/public/images/Horace Slughorn.webp',
+    'Madam Hooch' : '/public/images/Madam Hooch.jpg',
+    'Madam Pomfrey' : '/public/images/Madam Pomfrey.webp',
+    'Minerva McGonagall' : '/public/images/Minerva McGonagall.jpg',
+    'Mrs Norris' : '/public/images/Mrs Norris.jpg',
+    'Quirinus Quirrel' : '/public/images/Quirinus Quirrel.jpg',
+    'Remus Lupin' : '/public/images/Remus Lupin.jpg',
+    'Rubeus Hagrid' : '/public/images/Rubeus Hagrid.webp',
+    'Septima Vector' : '/public/images/Septima Vector.jpg',
+    'Sybill Trelawney' : '/public/images/Sybill Trelawney.webp',
+    'Wilhelmina Grubbly-Plank' : '/public/images/Wilhelmina Grubbly-Plank.jpg',
+}
 
 const HogwartsStaff = () => { 
     
@@ -9,6 +37,8 @@ const [loading, setLoading] = useState(false)
 const [errorMsg, setErrorMsg] = useState(null)
 const [input, setInput] = useState('')
 const [filteredData, setFilteredData] = useState([])
+const [favoriteStaff, setFavoriteStaff] = useState([])
+
 
 const fetchData = async () => {
     try {
@@ -57,8 +87,12 @@ return (
     {errorMsg && <p className='text-red-600'>{errorMsg}</p>}
     {loading && <div className='w-screen h-screen flex items-center justify-center'><RingLoader size={150}/> </div>}
     {
-        filteredData.map((item, index) => (
-            <StaffCard key={index} item={item} />
+        filteredData.map((item) => (
+            <div className='border p-4 bg-gray-100 rounded-lg shadow-lg'>
+            <h2 className='text-sm'>{item.name}</h2>
+            <img src={characterObject[item.name]} alt={item.name} className='object-center rounded-md size-96'/>
+            <button className={favoriteStaff ? 'p-2 text-red-600' : 'p-2 text-gray-600'}>{favoriteStaff ? <FaHeart/> : <FaRegHeart/> }</button>
+          </div>
         ))}   
 </div>
 </div>)}
